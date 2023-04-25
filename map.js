4cdf6432f66f3e29fafd0b1199136b3ca9b28d69
GalaxyShard <swift.galaxyshard@gmail.com>
2023-04-25 11:46:48 -0400

    added scrolling & minimal tutorial

--- map.js
+++ map.js
@@ -3,9 +3,59 @@ let map = document.getElementById("map");
 
 // TODO
 // possibly add random lines to the sides, or connect the icons
-// add the scrolling feature
 // add sections or headers somehow for projects
 // possibly lay out the map like the interactive version, from a top-down perspective
+
+function dragMap(dx, dy) {
+    let x = parseFloat(map.style.getPropertyValue("--map-offset-x") || 0);
+    let y = parseFloat(map.style.getPropertyValue("--map-offset-y") || 0);
+    
+    let scale = 1;
+    map.style.setProperty("--map-offset-x", `${x + dx*scale}`);
+    map.style.setProperty("--map-offset-y", `${y + dy*scale}`);
+
+}
+function handleMouseMove(e) {
+    dragMap(e.movementX, e.movementY);
+}
+let lastTouchX = 0;
+let lastTouchY = 0;
+function handleTouchMove(e) {
+    let touchX = 0;
+    let touchY = 0;
+    for (let touch of e.changedTouches) {
+        touchX += touch.clientX;
+        touchY += touch.clientY;
+    }
+    dragMap(touchX - lastTouchX, touchY - lastTouchY);
+    lastTouchX = touchX;
+    lastTouchY = touchY;
+}
+function handleMouseUp(e) {
+    map.removeEventListener("mousemove", handleMouseMove);
+}
+function handleTouchEnd(e) {
+    map.removeEventListener("touchmove", handleTouchMove);
+}
+map.addEventListener("mousedown", e => {
+    e.preventDefault();
+    map.addEventListener("mousemove", handleMouseMove);
+});
+map.addEventListener("mouseup", handleMouseUp);
+map.addEventListener("mouseleave", handleMouseUp);
+map.addEventListener("touchstart", e => {
+    map.addEventListener("touchmove", handleTouchMove);
+    lastTouchX = 0;
+    lastTouchY = 0;
+    for (let touch of e.changedTouches) {
+        lastTouchX += touch.clientX;
+        lastTouchY += touch.clientY;
+    }
+});
+map.addEventListener("touchend", handleTouchEnd);
+map.addEventListener("touchcancel", handleTouchEnd);
+
+
 
 function createIcon(name, offsetX, offsetY, popup) {
     let container = document.createElement("div");
@@ -16,7 +66,7 @@ function createIcon(name, offsetX, offsetY, popup) {
     container.style.setProperty("--y", `${offsetY}`);
 
     button.addEventListener("click", () => {
-        openPopup(popup.subpage, popup.extWebsite);
+        openPopup(popup.subpage, popup.extWebsite, popup.closedEvent);
     });
     
     title.textContent = name;
@@ -25,11 +75,26 @@ function createIcon(name, offsetX, offsetY, popup) {
     container.appendChild(button);
     container.appendChild(title);
     map.appendChild(container);
+    return { container: container, button: button, title: title };
 }
-createIcon("Click here", 0, 0, {
-    subpage: "subpages/click-here.html"
+let clickHere = createIcon("Click here", 0, 0, {
+    subpage: "subpages/click-here.html",
+    closedEvent: () => {
+        localStorage.setItem("map-tutorial", true);
+        createMap();
+    },
 });
+let mapCreated = false;
+function createMap() {
+    if (mapCreated) {
+        return;
+    }
+    mapCreated = true;
+    createIcon("About", -5, 6, {
+        subpage: "subpages/about.html"
+    });
+}
 
-createIcon("About", -5, 6, {
-    subpage: "subpages/about.html"
-});
+if (localStorage.getItem("map-tutorial")) {
+    createMap();
+}
--- popup.js
+++ popup.js
@@ -3,16 +3,21 @@ const closePopup = popup.getElementsByTagName("button")[0];
 const newTab = popup.getElementsByTagName("a")[0];
 const popupFrame = popup.getElementsByTagName("iframe")[0];
 
+let closedEvent;
 closePopup.addEventListener("click", _ => {
     popup.classList.remove("opened");
+    if (closedEvent) {
+        closedEvent();
+    }
 });
 
-function openPopup(subpage, extWebsite) {
+function openPopup(subpage, extWebsite, onClose) {
     if (popup.classList.contains("opened")) {
         return;
     }
     popupFrame.src = subpage;
-
+    closedEvent = onClose;
+    
     // disable "Open in new tab" if there is no dedicated website
     newTab.href = extWebsite || "javascript:void()";
     if (extWebsite) {
