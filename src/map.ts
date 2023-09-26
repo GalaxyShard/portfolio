457d855857df49d5bfa26c577e50949ec5ddfd0b
GalaxyShard <galaxyshard.tech@proton.me>
2023-09-26 11:54:20 -0400

    experimental: impossible triangle layout

--- src/map.ts
+++ src/map.ts
@@ -63,7 +63,7 @@ function createContainer(className: string, offset: Vec2) {
     container.style.setProperty("--y", `${offset[1]}`);
     return container;
 }
-function createIcon(name: string, offset: Vec2, popup: { subpage: any; closedEvent?: any; /*extWebsite?: any;*/ }) {
+function createIcon(name: string, offset: Vec2, popup: { subpage: string; closedEvent?: any }) {
     let container = createContainer("icon", offset);
     let button = document.createElement("button");
     let title = document.createElement("div");
@@ -121,37 +121,88 @@ async function createMap() {
     const project2Pos: Vec2 = [4, 15];
     const resumePos: Vec2 = [0, -6];
 
-    await delay(delayTime);
-    createLine(startPos, aboutPos);
-    createLine(startPos, project0Pos);
-    createLine(startPos, resumePos);
+    const triangle: Vec2[] = [
+        [-1.56, 10], [1.23, 10], // 0, 1
+        [-1.52, 5.202], // 2
+        [-0.2, 2.24], // 3
+        [-2.1, -2.19], [2.9, -1.595], [6.3, -2.19], // 4, 5, 6
+        [-9.8, -4.595], [-3.9, -4.45], [10.66, -4.45], // 7, 8, 9
+        [-8.33, -6.89], [9.25, -6.89], // 10, 11
+    ];
+    //           0---1
+    //         /  \   \
+    //        /    \   \
+    //       /   2  \   \
+    //      /   / \  \   \
+    //     /   /   3  \   \
+    //    /   /   / \  \   \
+    //   /   /   4---5--6   \
+    //  /   /   /            \
+    // 7   /   8--------------9
+    //  \ /                  /
+    //   10----------------11
+    const lines: number[] = [
+        2, 5,
+        3, 8,
+        4, 6,
+        2, 3,
+        4, 8,
+        5, 6,
 
-    await delay(delayTime);
-    createIcon("About", aboutPos, {
-        subpage: "subpages/about.html"
-    });
-    createIcon("Nua", project0Pos, {
-        subpage: "subpages/nua.html",
-    });
-    createIcon("Resumé", resumePos, {
-        subpage: "subpages/resume.html"
-    });
+        2, 10,
+        8, 9,
+        0, 6,
+        7, 10,
+        10, 11,
+        9, 11,
+        1, 9,
+        0, 1,
+        0, 7,
+    ]
+    function getPos(index: number) : Vec2 {
+        let scale = 2.5;
+        return [triangle[index][0] * scale, triangle[index][1] * scale];
+    }
+    const mapIcons: {[index: number]: {name:string, subpage:string, closedEvent?:any}} = {
+        0: {
+            name: "Cups & Pups",
+            subpage: "subpages/cups-pups.html",
+        },
+        8: {
+            name: "Retro Remake",
+            subpage: "subpages/retro-remake.html",
+        },
+        3: {
+            name: "About",
+            subpage: "subpages/about.html",
+        },
+        4: {
+            name: "Resumé",
+            subpage: "subpages/resume.html",
+        },
+        7: {
+            name: "Nua",
+            subpage: "subpages/nua.html",
+        },
+    };
+    let initializedIcons: {[index: number]: boolean} = {};
+    for (let i = 0; i < lines.length; i += 2) {
+        await delay(delayTime);
+        let a_index = lines[i];
+        let b_index = lines[i+1];
+        let a: Vec2 = a_index == -1 ? [0,0] : getPos(a_index);
+        let b: Vec2 = b_index == -1 ? [0,0] : getPos(b_index);
+        createLine(a, b);
 
-    await delay(delayTime);
-    createLine(project0Pos, project1Pos);
-
-    await delay(delayTime);
-    createIcon("Retro Remake", project1Pos, {
-        subpage: "subpages/retro-remake.html",
-    });
-
-    await delay(delayTime);
-    createLine(project1Pos, project2Pos);
-
-    await delay(delayTime);
-    createIcon("Cups & Pups", project2Pos, {
-        subpage: "subpages/cups-pups.html",
-    });
+        if (initializedIcons[a_index] != true && mapIcons[a_index]) {
+            createIcon(mapIcons[a_index].name, a, { subpage: mapIcons[a_index].subpage, closedEvent: mapIcons[a_index].closedEvent });
+        }
+        if (initializedIcons[b_index] != true && mapIcons[b_index]) {
+            createIcon(mapIcons[b_index].name, b, { subpage: mapIcons[b_index].subpage, closedEvent: mapIcons[b_index].closedEvent });
+        }
+        initializedIcons[a_index] = true;
+        initializedIcons[b_index] = true;
+    }
 }
 
 if (localStorage?.getItem("map-tutorial")) {
